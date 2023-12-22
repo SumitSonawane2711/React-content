9ea32adcfe0836c8ba401f6e891c42630313c1dd
Sumit <sumitrs2711@gmail.com>
2023-12-22 16:17:01 +0530

    useEffect,useRef and useCallback hooks

---
+++ 05passwordGenerator/README.md
@@ -0,0 +1,19 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+till understand :
+
+useCallback : memorised the overall function or part of the function. the values which are memorised by the hook depneds on dependencies privide in the defination.
+
+useEffect : this called first when the page is reload. it rerun whenever any dependencies changed.
+
+to copy text in the clipboard we used, window.navigator.clipboard.writeText(pass); method
+
+useRef : used to pass the reference to variable
+
---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,102 @@
+import { useState, useCallback,useEffect,useRef } from 'react'
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [number, setNumber] = useState(false)
+  const [charactor, setCharactor] = useState(false)
+  const [password,setPassword] = useState("")
+  const passwordRef = useRef(null)
+  
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(number) str += "0123456789"
+    if(charactor) str += "!@#$%^&*()_-+={}[]~`'"
+
+    for (let i = 1; i <= length; i++){
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  },[length, number, charactor,setPassword ])
+
+  const copyPasswordToClipboard = useCallback(()=> {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,100);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  }, [length,number,charactor,passwordGenerator])
+
+
+  return (
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md bg-gray-800 rounded-lg py-5 px-4 my-8 text-orange-600'>
+        <h1 className='text-white text-center my-3 mt-1'>Password Generator</h1>
+        
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+           type="text"
+           value={password}
+           className='outline-none w-full py-1 px-3'
+           placeholder='password'
+           readOnly
+           ref={passwordRef}
+           />
+          <button
+           onClick={copyPasswordToClipboard}
+           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+            copy
+          </button>  
+        </div>
+       
+       <div className='flex text-sm gap-x-2'>
+        
+        <div className='flex items-center gap-x-1'>
+          <input 
+           type="range"
+           min={6}
+           max={100}
+           value={length}
+           className='cursor-pointer'
+           onChange={(e) => {setLength(e.target.value)}} 
+          />
+          <label>Length: {length}</label>
+        </div>
+
+        <div className='flex items-center gap-x-1'>
+          <input
+           type="checkbox"
+           defaultChecked={number}
+           id='numberInput'
+           onChange={() => {setNumber((prev) => !prev)}} 
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+
+        <div className='flex items-center gap-x-'>
+          <input
+           type="checkbox"
+           defaultChecked={charactor}
+           id='charactorInput'
+           onChange={() => {setCharactor((prev) => !prev)}} 
+          />
+          <label htmlFor="charactorInput">Charactors</label>
+        </div>
+       
+       </div> 
+      
+      </div>
+      
+
+    </>
+  )
+}
+
+export default App

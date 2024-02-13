ae972631887080d1a71e2ada467863e4932d1866
Sumit <sumitrs2711@gmail.com>
2024-02-14 00:26:38 +0530

    build a sign-in form

--- 14SnapGram/src/context/AuthContext.jsx
+++ 14SnapGram/src/context/AuthContext.jsx
@@ -57,11 +57,11 @@ const AuthProvider = ({children}) => {
     };
 
     useEffect(()=>{
-         //localStorage.getItem('cookieFallback') === null
+         //
 
         if(
-            localStorage.getItem('cookieFallback') ==='[]'
-           
+            localStorage.getItem('cookieFallback') ==='[]' ||
+            localStorage.getItem('cookieFallback') === null
         ) navigate('/sign-in')
 
         checkAuthUser();
